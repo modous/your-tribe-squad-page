46be04eaca654db5d361d6110a108ae6e471c8bf
Ivar Schuyt <112855849+IvarSchuyt@users.noreply.github.com>
2022-09-14 11:22:41 +0200

    lijntjes en namen

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,11 @@
-// Retreiving all card elemenets.
 const cardElement = document.querySelector(".js_card");
 const nameElement = document.querySelector(".js_name");
 const imagelement = document.querySelector(".js_image");
 
-// Looping through all li's and adding a click event listener.
 document.querySelectorAll(".js_bin li").forEach((element) => {
   element.addEventListener("click", (event) => {
     const name = event.target.innerText;
 
-    // If card is already visible, remove the animation and back after 1 second. Then return.
     if (cardElement.classList.contains("-animate")) {
       cardElement.classList.remove("-animate");
 
@@ -18,7 +15,7 @@ document.querySelectorAll(".js_bin li").forEach((element) => {
 
       setTimeout(() => {
         cardElement.classList.add("-animate");
-      }, 1000);
+      }, 600);
 
       return;
     }
@@ -32,10 +29,8 @@ document.querySelectorAll(".js_bin li").forEach((element) => {
 });
 
 function updateCard(name) {
-  // Update card text with new name.
   nameElement.innerText = name;
 
-  // Update card image source and alt text.
   imagelement.src = `./assets/${name.toLowerCase()}.webp`;
   imagelement.alt = `Portret van ${name}`;
 }
